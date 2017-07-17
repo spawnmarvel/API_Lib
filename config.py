21534d897981cc2cc6a2d3ef07c87ab9fd46464a
Jan Espen Kleivane <espenkleivane@gmail.com>
2017-07-17 15:16:30 +0200

    with auth clean 4

--- config.py
+++
@@ -1,5 +0,0 @@
-DEBUG = True
-SECRET_KEY = "fttftf122323434343<<a12784596!@"
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password_lalala@localhost/pydatabase'
-
